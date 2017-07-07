92e95932321b825e223d12ee4111f08b043968c3
guidovitafinzi <guido.vitafinzi@gmail.com>
2017-07-07 13:57:48 +0100

    Added available bikes failure condition

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,24 @@
 require 'docking_station'
 
 describe DockingStation do
-	it 'responds to release_bike' do
-		expect(subject).to respond_to :release_bike
+
+	describe "#release_bike"do
+		it 'release a bike' do
+			bike = Bike.new
+			subject.dock(bike)
+			expect(subject.release_bike).to eq bike
+		end
+		it "raises an error when there are no bikes available" do
+			expect {subject.release_bike}.to raise_error "No bikes available"
+		end
 	end
 
-	it 'releases working bikes' do
-		bike = subject.release_bike
-		expect(bike).to be_working
-	end
+	#it 'releases working bikes' do
+		#bike = Bike.new
+		#subject.dock(bike)
+		#bike = subject.release_bike
+		#expect(bike).to be_working
+	#end
 
 	it { is_expected.to respond_to(:dock).with(1).argument }
 
