1d0180da8a69b7147ea74a1ab5978cace6e94506
guidovitafinzi <guido.vitafinzi@gmail.com>
2017-07-11 15:39:33 +0100

    Added broken bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,4 +3,9 @@ describe Bike do
 	it "respods to working?" do
 		expect(subject).to respond_to :working?
 	end
+
+	it 'can be reported broken' do
+		subject.report_broken
+		expect(subject).to be_broken
+	end
 end
