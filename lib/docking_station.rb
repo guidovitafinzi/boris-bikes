ad8b208fe081b86b6c7502dbac8b5893cf1c04ed
guidovitafinzi <guido.vitafinzi@gmail.com>
2017-07-11 15:33:34 +0100

    Added initialize station capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,28 +3,32 @@ require_relative 'bike'
 class DockingStation
 
 	DEFAULT_CAPACITY = 20
+	attr_accessor :capacity
 
-	def initialize
+	def initialize(capacity=DEFAULT_CAPACITY)
 		@bikes = []
+		@capacity = capacity
 	end
 
 	def release_bike
 		fail 'No bikes available' if empty?
-		@bikes.pop
+		bikes.pop
 	end
 
 	def dock(bike)
 		fail 'Docking station full' if full?
-		@bikes << bike
+		bikes << bike
 	end
 
 	private
 
+	attr_reader :bikes
+
 	def full?
-		@bikes.count >= DEFAULT_CAPACITY
+		bikes.count >= capacity
 	end
 
 	def empty?
-		@bikes.empty?
+		bikes.empty?
 	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -32,5 +32,21 @@ describe DockingStation do
 			subject.dock(bike)
 			expect(subject.bike).to eq bike
 		end
+
+		it 'has a default capacity' do
+			expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+		end
+	end
+	
+	describe "initialization" do
+		subject { DockingStation.new}
+		let(:bike) { Bike.new }
+		it 'has a variable capacity' do
+			described_class::DEFAULT_CAPACITY.times do
+				subject.dock(bike)
+			end
+			expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+
+		end
 	end
 end	
